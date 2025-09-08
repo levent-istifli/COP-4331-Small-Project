3bf40e4cf53ef63377f58488c8d3c05519337b11
Levent Istifli <levent.istifli@ucf.edu>
2025-09-08 17:26:20 -0400

    Added delete contact endpoint

---
+++ LAMPAPI/delete_contact.php
@@ -0,0 +1,52 @@
+<?php
+    require __DIR__ . "/../config.php";
+
+    // gets JSON request info
+    $in_data = json_decode(file_get_contents('php://input'), true);
+
+    // connect to my sql database
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+    // checks if there is any connection issue. Performs the "Add User" operation
+    if($conn->connect_error) {
+        returnWithError($conn->connect_error);
+    } else {
+
+        // input validation on server side
+        $required = ["userid", "id"];
+        
+        foreach($required as $field) {
+            if(!isset($in_data[$field]) || trim($in_data[$field]) === "") {
+                returnWithError("Missing or Empty Field: $field");
+                $conn->close();
+                exit();
+            }
+        } 
+
+        // prepares statements to add user. checks if user already exists. add user to db if user does
+        // not exists, otherwise returns error message
+        $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID = ? AND UserID = ?");
+        $stmt->bind_param("ii", $in_data["id"], $in_data["userid"]);
+
+        try {
+            $stmt->execute();
+            returnWithError("");
+
+        } catch(mysqli_sql_exception $exception) {
+           returnWithError($exception->getMessage());
+        }
+
+		$stmt->close();
+		$conn->close();
+    }
+
+    function sendResultInfoAsJson($obj) {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err) {
+        $ret = '{"Error":"'. $err . '"}';
+        sendResultInfoAsJson($ret);
+    }
+?>
